a9a9601d32ea6127f6a2808e8016ef5db1529473
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2020-04-17 19:57:42 -0300

    Módulo cors

--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 module.exports = app => {
     // settings
     app.set('port', process.env.PORT || 3000)
 
     // middlewares
+    app.use(cors());
     app.use(express.json());
 
 }
